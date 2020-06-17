a4fd3a7f8dbc2e4e7b6daf275166fea49e80c65c
Liu Wei 刘威 (690) <liuwei.psi@haier.com>
2020-06-17 18:19:38 +0800

    addRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Hello from './components/hello'
-
+import { Router, Route } from 'react-router'
+import createBrowserHistory from '../history';
+const browserHistory = createBrowserHistory()
 ReactDOM.render (
-  <Hello value="123"/>,
+  <Router history={browserHistory}>
+    <Route path="/" component={Hello}>
+      {/* <Route path="about" component={About} />
+      <Route path="inbox" component={Inbox}>
+        <Route path="messages/:id" component={Message} />
+      </Route> */}
+    </Route>
+  </Router>,
   document.getElementById("root")
 )
